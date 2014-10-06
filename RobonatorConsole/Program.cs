c6b7e114f3372767e23751a17042c6c92eab8727
byrakham <byrakham@gmail.com>
2014-10-06 22:29:04 +0300

    fixed programm

--- RobonatorConsole/Program.cs
+++ RobonatorConsole/Program.cs
@@ -10,6 +10,8 @@ namespace RobonatorConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("HEllo ROBONATOR!!");
+            Console.ReadLine();
         }
     }
 }
